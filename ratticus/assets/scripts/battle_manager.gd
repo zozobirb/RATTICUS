db3a2d1c49148c235cc528601804f9e2c99f7ccc
RosePinkTerminal <elhabashyrose@gmail.com>
2025-03-29 15:28:54 -0500

    fixed collsion

--- ratticus/assets/scripts/battle_manager.gd
+++ ratticus/assets/scripts/battle_manager.gd
@@ -3,5 +3,5 @@ class_name battle_manager
 
 func battle_time():
 	
-	
+	print("in the script, straight up printing it")
 	pass
--- ratticus/assets/scripts/spawn_areas.gd
+++ ratticus/assets/scripts/spawn_areas.gd
@@ -2,18 +2,19 @@ extends Area2D
 class_name SpawnAreas
 
 
-var _player = null
+var _player := Player
 
 func _ready():
 	randomize()
 	
-func _on_body_entered():
-	_roll_chance()
-	
 
+func _on_area_entered(area: Area2D) -> void:
+	print("IM IN DA GRASS")
+	_roll_chance()
 
 func _roll_chance():
 	var probability : int = _player.encounterChance 
 	if (randi() % probability) == (probability - 1): 
+		print("i solo this ez")
 		gameManager.battle_time()
 		pass
