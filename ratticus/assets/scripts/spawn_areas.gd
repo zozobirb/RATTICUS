538b6b60885f9b7292e2f431b6531ad306c80e89
RosePinkTerminal <elhabashyrose@gmail.com>
2025-03-29 17:14:27 -0500

    balls

--- ratticus/assets/scripts/spawn_areas.gd
+++ ratticus/assets/scripts/spawn_areas.gd
@@ -12,8 +12,13 @@ func _on_area_entered(area: Area2D) -> void:
 	_roll_chance()
 
 func _roll_chance():
-	var probability : int = 10 
+	var probability : int = 5
 	if (randi() % probability) == (probability - 1): 
 		print("i solo this ez")
+		$"../Player".global_position = $"../teleport".global_position
+		swap_cam()
 		gameManager.battle_time()
 		pass
+
+func swap_cam():
+	$"../camera2".make_current()
